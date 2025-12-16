70b0fe5d0fade31e949428f743f6e70400bcdc9d
tranhuong0204 <huongtran4002@gmail.com>
2025-12-17 00:09:19 +0700

    user's courses

--- backend/payment/src/main/java/com/hdv/payment/config/SecurityConfig.java
+++ backend/payment/src/main/java/com/hdv/payment/config/SecurityConfig.java
@@ -1,7 +1,7 @@
 package com.hdv.payment.config;
 
 //import com.example.onlineCourses.jwt.JwtAuthenticationFilter;
-import com.hdv.payment.jwt.JwtAuthenticationFilter;
+//import com.hdv.payment.jwt.JwtAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
